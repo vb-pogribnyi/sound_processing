ad318154f38cf71425bc22e4f53690022800d276
Vitalii Pogribnyi <vb.pogribnyi@gmail.com>
2026-04-23 11:55:23 +0500

    Adapted for scaling

--- src/exporter.py
+++ src/exporter.py
@@ -1,11 +1,12 @@
 import os
 import yaml
+import json
 import shutil
 import numpy as np
 from scipy.spatial.transform import Rotation
 import cv2 as cv
 
-def export(experiment, mics, sensors_direction, angle_step, debub_file=None):
+def export(experiment, mics, sensors_direction, angle_step, debug_file=None):
     exp_descr_path = os.path.join('/app/Experiments', experiment, 'experiment.yml')
     exp_output_path = os.path.join('/app/Experiments', experiment, 'outputs')
     assert os.path.exists(exp_descr_path), "Invalid experiment"
@@ -31,92 +32,128 @@ def export(experiment, mics, sensors_direction, angle_step, debub_file=None):
     size_y = exp_descr['export']['y_end'] - exp_descr['export']['y_start']
     size_z = exp_descr['export']['z_end'] - exp_descr['export']['z_start']
 
+    mics_xz -= np.expand_dims(np.mean(mics_xz, axis=2), 2)
+    result = {}
     # TODO: do the following in the loop, iterate over all possible sensor origins
     sensors_origin_x = size_x / 1.6 + exp_descr['export']['x_start']
     sensors_origin_y = size_y / 2 + exp_descr['export']['y_start']
     sensors_origin_z = size_z / 1.4 + exp_descr['export']['z_start']
+    key_position = (sensors_origin_x, sensors_origin_y, sensors_origin_z)
+    result[key_position] = {}
     source_directions = []
     for source in exp_descr['sources']:
-        source_size = 128 # TODO: load from meta.json
-        source_size_z = 16 # TODO: load from meta.json
-        dx = source['x'] + source_size / 2 - sensors_origin_x
-        dy = source['y'] + source_size / 2 - sensors_origin_y
-        dz = source['z'] + source_size_z / 2 - sensors_origin_z
+        mesh_meta = json.load(open(os.path.join('/app/input_mesh', source['type'], 'meta.json'))) 
+        dx = source['x'] + mesh_meta['nx'] / 2 - sensors_origin_x
+        dy = source['y'] + mesh_meta['ny'] / 2 - sensors_origin_y
+        dz = source['z'] + mesh_meta['nz'] / 2 - sensors_origin_z
         source_directions.append((dx, dy, dz))
+    
+    for rot_z in range(len(angles)):
+        for rot_x in range(len(angles)):
+            key_rotation = (angles[rot_z], angles[rot_x])
+            array_snapshot = {}
+            array_snapshot['sensor_direction_zx'] = directions_z[rot_z][0][0]
+            array_snapshot['sensor_direction_zy'] = directions_z[rot_z][0][1]
+            array_snapshot['sensor_direction_xz'] = directions_x[rot_x][0][2]
+            array_snapshot['sensor_direction_xy'] = directions_x[rot_x][0][1]
+            array_snapshot['mic_positions'] = [
+                    [round(mic[0] / exp_descr['mesh']['dx'] + sensors_origin_x),
+                    round(mic[1] / exp_descr['mesh']['dy'] + sensors_origin_y),
+                    round(mic[2] / exp_descr['mesh']['dz'] + sensors_origin_z)]
+                for mic in mics_xz[rot_x, rot_z]
+            ]
+            array_snapshot['sources'] = []
+            for source in source_directions:
+                xa = np.array([array_snapshot['sensor_direction_xz'], array_snapshot['sensor_direction_xy'], 0])
+                xb = np.array([source[2], source[1], 0])
+                cos_anglex = np.dot(xa, xb) / (np.linalg.norm(xa) * np.linalg.norm(xb))
+                anglex = np.degrees(np.arccos(np.clip(cos_anglex, -1.0, 1.0)))
+                crossx = np.cross(xa, xb)
+                axis = [0, 0, 1]
+                if np.dot(crossx, axis) < 0:
+                    anglex = -anglex
+                
+                za = np.array([array_snapshot['sensor_direction_zx'], array_snapshot['sensor_direction_zy'], 0])
+                zb = np.array([source[0], source[1], 0])
+                cos_anglez = np.dot(za, zb) / (np.linalg.norm(za) * np.linalg.norm(zb))
+                anglez = np.degrees(np.arccos(np.clip(cos_anglez, -1.0, 1.0)))
+                crossz = np.cross(za, zb)
+                if np.dot(crossz, axis) < 0:
+                    anglez = -anglez
+                array_snapshot['sources'].append({
+                    'angle_x': anglex,
+                    'angle_z': anglez
+                })
 
-    if debub_file is not None:
+            result[key_position][key_rotation] = array_snapshot
+
+    if debug_file is not None:
+        sensors_origin = list(result.keys())[0]
+        result_item = result[sensors_origin]
         img_z = np.zeros((exp_descr['mesh']['size_y'], exp_descr['mesh']['size_x'], 3), np.uint8) + 25
-        mics_z = mics_xz[mics_xz.shape[0] // 2].copy()
-        mics_z -= np.expand_dims(np.mean(mics_z, axis=1), 1)
-        assert np.all([len(np.unique(mics_z[:, i, 2].round(4))) == 1 for i in range(mics_z.shape[1])]), "Wrong rotation"
-        for cidx, rot_i in enumerate([0, mics_z.shape[0]//4*3, mics_z.shape[0]//2]):
-            print('-----------------------------------')
-            rot_vectors = mics_z[rot_i]
-            for v in rot_vectors:
-                img_z[round(v[1] / exp_descr['mesh']['dy'] + sensors_origin_y), round(v[0] / exp_descr['mesh']['dx'] + sensors_origin_x), [cidx]] = 255
-            # Draw general direction of the array
-            line_color = [0, 0, 0]
-            line_color[cidx] = 255
-            sensor_direction_x = directions_z[rot_i][0][0]
-            sensor_direction_y = directions_z[rot_i][0][1]
-            cv.line(img_z, 
-                    (int(sensors_origin_x), int(sensors_origin_y)), 
-                    (int(sensors_origin_x + sensor_direction_x/exp_descr['mesh']['dx']), int(sensors_origin_y + sensor_direction_y/exp_descr['mesh']['dy'])), 
-                    line_color, 2)
-            for source in source_directions:
-                cv.line(img_z, 
-                        (int(sensors_origin_x), int(sensors_origin_y)), 
-                        (int(sensors_origin_x + source[0]), int(sensors_origin_y + source[1])), 
-                        line_color, 1)
-                a = np.array([sensor_direction_x, sensor_direction_y, 0])
-                b = np.array([source[0], source[1], 0])
-                cos_angle = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
-                angle = np.degrees(np.arccos(np.clip(cos_angle, -1.0, 1.0)))
-                cross = np.cross(a, b)
-                axis = [0, 0, 1]
-                if np.dot(cross, axis) < 0:
-                    angle = -angle
-                print("Angle:", angle)
-        # cv.imwrite(os.path.join(export_path, 'rots_z.png'), img_z)
+        for cidx, angle_z in enumerate([-90, 0, 30]):
+            array_snapshot = result_item[(angle_z, 0)]
+            for v in array_snapshot['mic_positions']:
+                img_z[v[1], v[0], [cidx]] = 255
 
-        # TODO: Is this a dublication?
-        img_x = np.zeros((exp_descr['mesh']['size_y'], exp_descr['mesh']['size_z'], 3), np.uint8) + 25
-        mics_x = mics_xz[:, mics_xz.shape[0] // 2].copy()
-        mics_x -= np.expand_dims(np.mean(mics_x, axis=1), 1)
-        assert np.all([len(np.unique(mics_x[:, i, 0].round(4))) == 1 for i in range(mics_x.shape[1])]), "Wrong rotation"
-        for cidx, rot_i in enumerate([mics_x.shape[0]//2-1, mics_x.shape[0]//2+1, mics_x.shape[0]//2]):
-            print('-----------------------------------')
-            rot_vectors = mics_x[rot_i]
-            for v in rot_vectors:
-                img_x[round(v[1] / exp_descr['mesh']['dy'] + sensors_origin_y), round(v[2] / exp_descr['mesh']['dz'] + sensors_origin_z), [cidx]] = 255
             # Draw general direction of the array
             line_color = [0, 0, 0]
             line_color[cidx] = 255
-            sensor_direction_z = directions_x[rot_i][0][2]
-            sensor_direction_y = directions_x[rot_i][0][1]
-            cv.line(img_x, 
-                    (int(sensors_origin_z), int(sensors_origin_y)), 
-                    (int(sensors_origin_z + sensor_direction_z/exp_descr['mesh']['dz']), int(sensors_origin_y + sensor_direction_y/exp_descr['mesh']['dy'])), 
+            sensors_direction = [
+                array_snapshot['sensor_direction_zx'],
+                array_snapshot['sensor_direction_zy'],
+                0
+            ]
+            cv.line(img_z, 
+                    (int(sensors_origin[0]), int(sensors_origin[1])), 
+                    (int(sensors_origin[0] + sensors_direction[0]/exp_descr['mesh']['dx']), int(sensors_origin[1] + sensors_direction[1]/exp_descr['mesh']['dy'])), 
                     line_color, 2)
-            for source in source_directions:
-                cv.line(img_x, 
-                        (int(sensors_origin_z), int(sensors_origin_y)), 
-                        (int(sensors_origin_z + source[2]), int(sensors_origin_y + source[1])), 
+            # Draw directions to the sources
+            for source in array_snapshot['sources']:
+                r = Rotation.from_euler('z', source['angle_z'], degrees=True)
+                rotated = r.apply(sensors_direction)*1000
+                cv.line(img_z, 
+                        (int(sensors_origin[0]), int(sensors_origin[1])), 
+                        (int(sensors_origin[0] + rotated[0]), int(sensors_origin[1] + rotated[1])), 
                         line_color, 1)
-                a = np.array([sensor_direction_z, sensor_direction_y, 0])
-                b = np.array([source[2], source[1], 0])
-                cos_angle = np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
-                angle = np.degrees(np.arccos(np.clip(cos_angle, -1.0, 1.0)))
-                cross = np.cross(a, b)
-                axis = [0, 0, 1]
-                if np.dot(cross, axis) < 0:
-                    angle = -angle
-                print("Angle:", angle)
+                print("AngleZ:", source['angle_z'])
+
+
+        print('-----------------------------')
+        # # TODO: Is this a dublication?
+        img_x = np.zeros((exp_descr['mesh']['size_y'], exp_descr['mesh']['size_z'], 3), np.uint8) + 25
+
+        for cidx, angle_x in enumerate([-5, 0, 5]):
+            array_snapshot = result_item[(0, angle_x)]
+            for v in array_snapshot['mic_positions']:
+                img_x[v[1], v[2], [cidx]] = 255
+
+            # Draw general direction of the array
+            line_color = [0, 0, 0]
+            line_color[cidx] = 255
+            sensors_direction = [
+                0,
+                array_snapshot['sensor_direction_zy'],
+                array_snapshot['sensor_direction_xz']
+            ]
+            cv.line(img_x, 
+                    (int(sensors_origin[2]), int(sensors_origin[1])), 
+                    (int(sensors_origin[2] + sensors_direction[2]/exp_descr['mesh']['dz']), int(sensors_origin[1] + sensors_direction[1]/exp_descr['mesh']['dy'])), 
+                    line_color, 2)
+            # Draw directions to the sources
+            for source in array_snapshot['sources']:
+                r = Rotation.from_euler('x', -source['angle_x'], degrees=True)
+                rotated = r.apply(sensors_direction)*1000
+                cv.line(img_x, 
+                        (int(sensors_origin[2]), int(sensors_origin[1])), 
+                        (int(sensors_origin[2] + rotated[2]), int(sensors_origin[1] + rotated[1])), 
+                        line_color, 1)
+                print("AngleX:", source['angle_x'])
         
         debug_img = np.zeros((img_z.shape[0], img_z.shape[1] + img_x.shape[1] + 5, 3), np.uint8)
         debug_img[:, :img_z.shape[1]] = img_z
         debug_img[:, img_z.shape[1] + 5:] = img_x
-        cv.imwrite(os.path.join(export_path, debub_file), debug_img)
+        cv.imwrite(os.path.join(export_path, debug_file), debug_img)
 
 
 if __name__ == "__main__":
