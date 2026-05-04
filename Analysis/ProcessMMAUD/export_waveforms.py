747d53abb12e54c7db4fca036cac204733d7c016
Vitalii Pogribnyi <vb.pogribnyi@gmail.com>
2026-05-04 10:24:21 +0300

    Removed sync between waveforms (itimestamps not precise enough)

--- Analysis/ProcessMMAUD/export_waveforms.py
+++ Analysis/ProcessMMAUD/export_waveforms.py
@@ -6,6 +6,7 @@ import numpy as np
 from tqdm import tqdm
 from pydub import AudioSegment  # pip install pydub audioop-lts
 from scipy.io.wavfile import write
+from scipy.interpolate import interp1d
 # AudioSegment.converter = 'C:\\Users\\vbpoh\\Documents\\Dojo\\PhD\\REPOS_mmaud_ros\\process\\ffmpeg-master-latest-win64-gpl-shared\\bin\\ffmpeg.exe'
 # AudioSegment.ffprobe = 'C:\\Users\\vbpoh\\Documents\\Dojo\\PhD\\REPOS_mmaud_ros\\process\\ffmpeg-master-latest-win64-gpl-shared\\bin\\ffprobe.exe'
 
@@ -21,56 +22,53 @@ audio_topics = [
 
 def process_file(audio_file, gt_file):
     audio_mp3 = pickle.load(open(audio_file, 'rb'))
-    # gt = pickle.load(open(gt_file, 'rb'))['/leica/point/relative']
+    gt = pickle.load(open(gt_file, 'rb'))['/leica/point/relative']
+    gt_t = np.array([i['timestamp'] for i in gt])
+    gt_x = np.array([i['x'] for i in gt])
+    gt_y = np.array([i['y'] for i in gt])
+    gt_z = np.array([i['z'] for i in gt])
+    f_gtx = interp1d(gt_t, gt_x, kind='linear')
+    f_gty = interp1d(gt_t, gt_y, kind='linear')
+    f_gtz = interp1d(gt_t, gt_z, kind='linear')
     np_buffers = {t: [] for t in audio_topics}
     timestamps = {t: [] for t in audio_topics}
     waveforms = {t: [] for t in audio_topics}
-    samples_written = {t: 0 for t in audio_topics}
-    # last_timestamp = 0
-    # gt_i = 0
-    # samples_written = 0
-    # timestamp_start = max([audio_mp3[t][0]['timestamp'] for t in audio_topics])
-    # timestamp_start = max(timestamp_start, gt[0]['timestamp'])
-    # timestamp_end = min([audio_mp3[t][-1]['timestamp'] for t in audio_topics])
-    # timestamp_end = min(timestamp_end, gt[-1]['timestamp'])
     idx_end = min([len(audio_mp3[t]) for t in audio_topics])
 
     for i in tqdm(range(idx_end)):
         for t in audio_topics:
             np_buffers[t].append(audio_mp3[t][i]['msg'])
-        if i < 3:
-            continue # That won't be able to read as mp3
-        for t in audio_topics:
-            buf = io.BytesIO(np.concatenate(np_buffers[t]).tobytes())
-            buf.seek(0)
-            audio = AudioSegment.from_file(buf, format='mp3')
-            samples = np.array(audio.get_array_of_samples())
+    for t in audio_topics:
+        buf = io.BytesIO(np.concatenate(np_buffers[t]).tobytes())
+        buf.seek(0)
+        audio = AudioSegment.from_file(buf, format='mp3')
+        samples = np.array(audio.get_array_of_samples())
 
-            # It produces 2-channel signal, but both channels are same:
-            # ch1 = samples[0::2]
-            # ch2 = samples[1::2]
-            # any(ch1 != ch2)  # False
-            # So I'm throwing away one channel
-            samples = samples[0::2]
-            waveforms[t].append(samples[samples_written[t]:])
-            timestamps[t].append(audio_mp3[t][i]['timestamp'])
-            print(audio_mp3[t][i]['timestamp'] // 1000000)
-            samples_written[t] = len(samples)
-    #     result[last_timestamp] = {
-    #         'timestamp': last_timestamp,
-    #         'samples': out_buffer,
-    #         'window': window,
-    #         'skips': n_timestamp_skips,
-    #         'gt_x': gt[gt_i]['x'],
-    #         'gt_y': gt[gt_i]['y'],
-    #         'gt_z': gt[gt_i]['z']
-    #     }
-    # # A quick double-check. Write raw signals as WAV
-    # wav_buffer = np.concatenate([result[item]['samples'] for item in result])
-    # write("dbg.wav", 48000, wav_buffer)
+        # It produces 2-channel signal, but both channels are same:
+        # ch1 = samples[0::2]
+        # ch2 = samples[1::2]
+        # any(ch1 != ch2)  # False
+        # So I'm throwing away one channel
+        # samples = samples[0::2]
+        waveforms[t] = samples[0::2]
+        timestamps[t] = np.linspace(audio_mp3[t][0]['timestamp'], audio_mp3[t][-1]['timestamp'], len(waveforms[t]))
+        
+    # The audio timestamps are not really synchronized, so I have to assume that first signal is correct 
+    # and the timestamps for the other ones match the first one.
+    gt_available = timestamps['/audio1/audio'] > min(gt_t)
+    gt_available = np.logical_and(gt_available, timestamps['/audio1/audio'] < max(gt_t))
+    for t in audio_topics:
+        waveforms[t] = waveforms[t][gt_available]
+        timestamps[t] = timestamps[t][gt_available]
+    gts = {
+        'x': f_gtx(timestamps['/audio1/audio']),
+        'y': f_gty(timestamps['/audio1/audio']),
+        'z': f_gtz(timestamps['/audio1/audio'])
+    }
     result = {
         "timestamps": timestamps,
-        "waveforms": waveforms
+        "waveforms": waveforms,
+        "ground_truth": gts
     }
     pickle.dump(result, open(audio_file[:-4] + '_decoded.pkl', 'wb'))
 
