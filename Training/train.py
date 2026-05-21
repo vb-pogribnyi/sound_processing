1191b4a7dd815e0ec30f761fdb3526f9eff5f8d5
Vitalii Pogribnyi <vb.pogribnyi@gmail.com>
2026-05-21 07:16:11 +0000

    Added visualizations for predictions

--- Training/train.py
+++ Training/train.py
@@ -4,7 +4,9 @@ import mlflow
 import argparse
 import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 from Data.loader import get_dataloader
+from NN.Cross3D.utils import sph2cart
 
 def load_model(model_name, is_spec, ch_in):
     if model_name == "conformer":
@@ -53,12 +55,13 @@ def do_train(train_ds_name, val_ds_name, preproc_name, model_name, run_name="Sou
                 # break
             if epoch % 1 == 0:
                 val_losses = []
+                vis_gts, vis_preds = [], []
                 for batch_id, (audio, doa) in enumerate(tqdm(val_dl)):
                     if batch_id == 0:
                         for example_id, vis_features in enumerate(audio):
                             vis_features = vis_features.cpu().detach().numpy()
                             if preproc_name == 'spec':
-                                vis_features = np.log(vis_features)
+                                vis_features = np.log(vis_features+1e-7)
                             vis_features -= vis_features.min()
                             vis_features /= vis_features.max()
                             vis_features *= 255
@@ -67,6 +70,11 @@ def do_train(train_ds_name, val_ds_name, preproc_name, model_name, run_name="Sou
                                 mlflow.log_image(map, artifact_file=f"map_{example_id}_{map_id}.png")
                     preds = model(audio.to(device))
                     gt = doa.unsqueeze(1).repeat(1, preds.shape[1], 1).to(device)
+                    for doa_item, pred_item in zip(gt, preds):
+                        doa_item = sph2cart(doa_item[0])
+                        pred_item = sph2cart(pred_item[0])
+                        vis_gts.append(doa_item.detach().cpu().numpy())
+                        vis_preds.append(pred_item.detach().cpu().numpy())
                     loss = mse(preds, gt.float())
                     val_losses.append(loss.item())
                     mean_val_loss = np.mean(val_losses)
@@ -82,6 +90,22 @@ def do_train(train_ds_name, val_ds_name, preproc_name, model_name, run_name="Sou
                         #     artifact_path="model",
                         #     registered_model_name=f"{model_name}_{epoch}" # Optional: register directly to model registry
                         # )
+
+                # Visualize predictions vs ground truth
+                vis_gts = np.array(vis_gts).T
+                vis_preds = np.array(vis_preds).T
+                fig = plt.figure(figsize=(12, 12))
+                ax = fig.add_subplot(111, projection='3d')
+                ax.scatter(vis_gts[0], vis_gts[1], vis_gts[2], color='crimson', label='Ground Truth', s=2)
+                ax.scatter(vis_preds[0], vis_preds[1], vis_preds[2], color='deepskyblue', label='Estimation', s=1)
+                ax.set_title(f'Predictions - ep {epoch}')
+                ax.grid(True)
+                # plt.savefig('/app/preds.png')
+                mlflow.log_figure(fig, f"preds_{epoch}.png")
+                plt.close()
+
+
+
                 print(epoch, np.mean(losses), mean_val_loss)
                 mlflow.log_metric("train_loss", np.mean(losses), epoch)
                 mlflow.log_metric("val_loss", mean_val_loss, epoch)
