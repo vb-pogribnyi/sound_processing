5bb1b17a299e0959f7c7e388c2cf297669466e68
Vitalii Pogribnyi <vb.pogribnyi@gmail.com>
2026-06-08 08:41:47 +0300

    ADC reader - SPI seems adequate.

--- Acquisition/FPGA/MCP3461Master.v
+++ Acquisition/FPGA/MCP3461Master.v
@@ -5,14 +5,169 @@ module MCP3461Master #(
     input wire i_INTERRUPT,
     input wire [NUM_ADC-1:0] i_MISO,
     // output wire sel_interrupt,
-    output wire o_MOSI,
-    output wire o_CS,
+    output reg o_MOSI,
+    output reg o_CS,
     output wire o_SCLK,
     output wire o_MCLK
 );
+reg [7:0] tx_buff [0:16];
+reg [7:0] tx_len = 0;
+reg [7:0] cfg [0:4];
+reg [3:0] byte_idx;
+reg [2:0] bit_idx;
+reg [7:0] tx_shift_reg;
+reg [31:0] rx_shift_reg [NUM_ADC-1:0];
+
+
+// DEBUG
+wire [31:0] rx_shift_reg_0 = rx_shift_reg[0];
+wire [31:0] rx_shift_reg_1 = rx_shift_reg[1];
+
+
+reg transmitting = 0;
+reg trigger = 0;
+reg sclk = 0;
+integer i;
+assign o_SCLK = sclk & !o_CS;
+localparam cmd_read_int = 8'h15;
+localparam cmd_read_val = 8'h10;
+localparam	TIMEOUT = 4;
+localparam	FRESH       = 3'h0,
+            ALIVE       = 3'h1,
+            // CONFIGURING = 3'h2,
+            READING     = 3'h3,
+            ERROR       = 3'h4;
+reg [2:0] state = FRESH;
 reg [1:0] cnt = 0;
+reg [16:0] to_cnt = TIMEOUT;
+initial begin
+    o_CS = 1;
+    cfg[0] = 8'hC3;  // CONFIG0. Internal ref, external MCLK, no current, conv mode
+    cfg[1] = 8'h00;  // CONFIG1. No prescaler, smallest oversampling
+    cfg[2] = 8'h45;  // CONFIG2. No current scale, no gain, no input chopping, yes ref chopping
+    cfg[3] = 8'hC0;  // CONFIG3. Continuous conversion, 16-bit coding, no CRC, no calibration
+    cfg[4] = 8'h00;  // IRQ. Enabled, no "start" interrupt
+    // Actually from this point on the default configuration is OK.
+    // cfg[5] = 8'h01;  // MUX. Measuring CH0 vs CH1
+    // cfg[6] = 8'h45;  // SCAN. 
+    // Fill configuration...
+end
+// SDI (MOSI) clocked on rising edge, update should be on falling edge
+// SDO (MISO) clocked out on falling edge, read should be on rising edge.
 assign o_MCLK = (cnt >= 2);
+reg [1:0] read_offset = 0;
 always @(posedge i_CLK50) begin
     cnt <= cnt + 1;
+    if (cnt == 0) begin // 6 MHZ clock
+        sclk <= !sclk;
+
+        // The state-machine sets transmission data and trigger, if required.
+        case(state)
+            FRESH: begin    // Just powered up, maybe not yet initialized
+                if (to_cnt > 0) begin  // HOOLD
+                    to_cnt <= to_cnt - 1;
+                end
+                else begin
+                    if (!trigger && !transmitting && tx_len == 0) begin
+                        tx_len <= 1;
+                        tx_buff[0] <= cmd_read_int;
+                        trigger <= 1;
+                    end
+                    if (!trigger && !transmitting && tx_len > 0) begin
+                        // TODO: Check if the answer is valid
+                        // That would mean the ADC is ready to talk.
+                        tx_len <= 0;
+                        state <= ALIVE;
+                    end
+                end
+            end
+            ALIVE: begin    // The ADC is ready to be configured
+                if (!trigger && !transmitting && o_CS && tx_len == 0) begin
+                    tx_len <= 5;
+                    tx_buff[0] <= cfg[0];
+                    tx_buff[1] <= cfg[1];
+                    tx_buff[2] <= cfg[2];
+                    tx_buff[3] <= cfg[3];
+                    tx_buff[4] <= cfg[4];
+                    trigger <= 1;
+                end
+                if (!trigger && !transmitting && tx_len > 0) begin
+                    // TODO: Check if the answer is valid
+                    // That would mean the ADC is configured.
+                    tx_len <= 0;
+                    state <= READING;
+
+                    to_cnt <= TIMEOUT;
+                end
+            end
+            READING: begin    // Main operation loop
+                if (to_cnt > 0) begin  // TODO: Wait for the interrupt to go low
+                    to_cnt <= to_cnt - 1;
+                end
+                else begin
+                    if (!trigger && !transmitting && o_CS && tx_len == 0) begin
+                        tx_len <= 3;    // 1 byte status 2 bytes ADC value
+                        tx_buff[0] <= cmd_read_val;
+                        tx_buff[1] <= 0;
+                        tx_buff[2] <= 0;
+                        trigger <= 1;
+                    end
+                    if (!trigger && !transmitting && o_CS && tx_len > 0) begin
+                        // TODO: Output the ADC value
+                        tx_len <= 0;
+                        to_cnt <= TIMEOUT;
+                    end
+                end
+            end
+        endcase
+        if (trigger && !transmitting) begin
+            transmitting <= 1;
+            trigger <= 0;
+            byte_idx <= 0;
+            bit_idx <= 7;
+            tx_shift_reg <= tx_buff[0];
+            read_offset <= 3;
+            
+            // rx_shift_reg <= 0;
+            for (i = 0; i < NUM_ADC; i = i + 1) begin
+                rx_shift_reg[i] <= 0;
+            end
+        end
+        if (transmitting) begin
+            if (sclk) begin // Falling edge; sclk will update to low at the end of posedge handler
+                tx_shift_reg <= {tx_shift_reg[6:0], 1'b0};
+                o_MOSI <= tx_shift_reg[7];
+                o_CS <= 0;
+                if (bit_idx == 0) begin
+                    byte_idx <= byte_idx + 1;
+                    tx_shift_reg <= tx_buff[byte_idx + 1];
+                    bit_idx <= 7;
+                end
+                else begin
+                    bit_idx <= bit_idx - 1;
+                end
+            end
+            else begin // Rising edge; sclk will update to high at the end of posedge handler
+                if (read_offset == 0) begin
+                    if (byte_idx < 4) begin
+                        for (i = 0; i < NUM_ADC; i = i + 1) begin
+                            rx_shift_reg[i] <= {rx_shift_reg[i][30:0], i_MISO[i]};
+                        end
+                    end
+                    if (bit_idx == 7) begin
+                        if (byte_idx == tx_len) begin
+                            transmitting <= 0;
+                        end
+                    end
+                end
+                else begin
+                    read_offset <= read_offset - 1;
+                end
+            end
+        end
+        else begin
+            o_CS <= 1;
+        end
+    end
 end
 endmodule
--- Acquisition/FPGA/MCP3461MasterTester.v
+++ Acquisition/FPGA/MCP3461MasterTester.v
@@ -2,35 +2,37 @@
 
 // iverilog .\Acquisition\FPGA\MCP3461Master.v .\Acquisition\FPGA\MCP3461MasterTester.v
 // vvp a.out  
+// gtkwave .\mcp3461.vcd
+
 // `include "Acquisition/FPGA/MCP3461Master.v"
 
 module MCP3461MasterTester;
 
-	// Inputs
-	reg i_CLK;
-	reg [1:0] miso;
-	reg interrupt;
+    // Inputs
+    reg i_CLK;
+    reg [1:0] miso;
+    reg interrupt;
 
-	// Outputs
-	wire o_MOSI;
-	wire o_MCLK;
-	wire o_SCLK;
-	wire o_RDY;
-	wire o_CS;
-	
-	wire [15:0] o_OUT1;
-	wire [15:0] o_OUT2;
-	
-	reg [15:0] value1 = 16'hACDC;
-	reg [15:0] value2 = 16'hBBDF;
-	
-	
-	integer i;
-	integer j;
-	always #5 i_CLK = ~i_CLK;
+    // Outputs
+    wire o_MOSI;
+    wire o_MCLK;
+    wire o_SCLK;
+    wire o_RDY;
+    wire o_CS;
+    
+    wire [15:0] o_OUT1;
+    wire [15:0] o_OUT2;
+    
+    reg [15:0] value1 = 16'hACDC;
+    reg [15:0] value2 = 16'hBBDF;
+    
+    
+    integer i;
+    integer j;
+    always #5 i_CLK = ~i_CLK;
 
-	// Instantiate the Unit Under Test (UUT)
-	MCP3461Master #( .NUM_ADC(2) ) uut (
+    // Instantiate the Unit Under Test (UUT)
+    MCP3461Master #( .NUM_ADC(2) ) uut (
         .i_CLK50(i_CLK),
         .i_INTERRUPT(interrupt),
         .i_MISO(miso),
@@ -38,7 +40,7 @@ module MCP3461MasterTester;
         .o_CS(o_CS),
         .o_SCLK(o_SCLK),
         .o_MCLK(o_MCLK)
-	);
+    );
 
     // input wire i_CLK50,
     // input wire i_INTERRUPT,
@@ -49,42 +51,29 @@ module MCP3461MasterTester;
     // output wire o_SCLK,
     // output wire o_MCLK
 
-	initial begin
+    initial begin
         $dumpfile("mcp3461.vcd");
         $dumpvars(0, MCP3461MasterTester);
-		// Initialize Inputs
-		i_CLK = 0;
-		miso[0] = 0;
-		miso[1] = 0;
-
-		// Wait 100 ns for global reset to finish
-		#100;
+        // Initialize Inputs
+        i_CLK = 0;
+        interrupt = 1;
+        #50;
         
-		// Add stimulus here
-		for (j = 15; j >= 0; j = j - 1)
-		begin
-			// @(posedge o_CONVST);
-			// i_MISO1 <= 1;
-			// i_MISO2 <= 1;
-			// #20 i_MISO1 <= 0;
-			// #20 i_MISO2 <= 0;
-			// //@(negedge o_SCLK);
-			// //i_MISO <= 0;
-			// //@(negedge o_SCLK);
-			// //i_MISO <= 1;
-			// for (i = 15; i >= 0; i = i - 1)
-			// begin
-			// 	@(negedge o_SCLK);
-			// 	if (i <= 15) begin
-			// 		i_MISO1 <= value1[i];
-			// 		i_MISO2 <= value2[i];
-			// 	end
-			// end
-		end
+        // Add stimulus here
+        for (j = 15; j >= 0; j = j - 1)
+        begin
+            for (i = 0; i < 17; i = i + 1) begin
+                @(negedge o_SCLK);
+                if (i > 0) begin
+                    miso[0] <= value1[15 - (i - 1)];
+                    miso[1] <= value2[15 - (i - 1)];
+                end
+            end
+        end
 
         $finish;
 
-	end
+    end
       
 endmodule
 
